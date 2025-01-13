8dc506580929d68c673418360b3c5510ec78c29c
Yue0925 <mellet1997@outlook.com>
2025-01-13 14:53:08 +0100

    BiqBIn

--- experiments/BiqBin/run.sh
+++ experiments/BiqBin/run.sh
@@ -2,39 +2,16 @@
 
 
 
-for file in ./instances_neg/*.txt; do
-    echo "$file ... "
-    julia run.jl "$file"
-done
-
-
-
-for file in ./instances_uni/*.txt; do
-    echo "$file ... "
-    julia run.jl "$file"
-done
-
-
-
-
-for file in ./instances_neqfloat/*.txt; do
-    echo "$file ... "
-    julia run.jl "$file"
-done
-
-
-
-
 # for file in ./instances_neg/*.txt; do
 #     echo "$file ... "
-#     julia tabWriter.jl "$file"
+#     julia run.jl "$file"
 # done
 
 
 
 # for file in ./instances_uni/*.txt; do
 #     echo "$file ... "
-#     julia tabWriter.jl "$file"
+#     julia run.jl "$file"
 # done
 
 
@@ -42,6 +19,29 @@ done
 
 # for file in ./instances_neqfloat/*.txt; do
 #     echo "$file ... "
-#     julia tabWriter.jl "$file"
+#     julia run.jl "$file"
 # done
+
+
+
+
+for file in ./instances_neg/*.txt; do
+    echo "$file ... "
+    julia tabWriter.jl "$file"
+done
+
+
+
+for file in ./instances_uni/*.txt; do
+    echo "$file ... "
+    julia tabWriter.jl "$file"
+done
+
+
+
+
+for file in ./instances_neqfloat/*.txt; do
+    echo "$file ... "
+    julia tabWriter.jl "$file"
+done
 
